721df1c18e4f6ec51e69b13046b641b966326e16
Alejandro Rincón Vera <alejandro.rv97@gmail.com>
2020-03-04 23:31:42 -0500

    Tird push, complete CRUD

--- src/routes/books.js
+++ src/routes/books.js
@@ -1,4 +1,4 @@
-const {Router} = require('express');
+const {Router} = require('express'); // Router-level middleware
 const router = Router();
 const underscore = require('underscore');
 
@@ -23,6 +23,24 @@ router.post('/', (req, res) => {
     // console.log(req.body);
     // res.send('Posted');
 });
+
+router.put('/:id', (req, res) => {
+    const{book_title, author, year, pages_count} = req.body;
+    const id = req.params.id;
+    if(book_title && author && year && pages_count){// if the body contains all the required values...
+        underscore.each(jsonBooks, (book, i) => {
+            if(book.id == id){
+                book.book_title = book_title;
+                book.author = author;
+                book.year = year;
+                book.pages_count = pages_count;
+                res.send(jsonBooks);
+            }
+            });
+        }else{
+            res.status(500).json({"error": "Maybe incorrect input, check if you passed all required values"});
+        }
+    });
 
 router.delete('/:id', (req, res) => {
     const {id} = req.params;
